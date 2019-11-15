a00e4ff3860502a8ac1382314cfaf8ab01192ccb
Tadeas Paule <tpaule.personal@gmail.com>
2019-11-15 16:44:24 +0000

    fixed resolution issues in executable

---
+++ Assets/GamePanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = Screen.height / 128;
+        transform.localScale = new Vector3(i,i,1f);
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
