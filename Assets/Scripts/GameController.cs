ff8933bd55c4e627c030dbd64924937293354f6c
Tadeas Paule <tpaule.personal@gmail.com>
2019-11-15 10:36:21 +0000

    WIP spells

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public SnakeController snakeController;
     public MapController mapController;
 
-    public TextMeshProUGUI highscoreText;
-    public TextMeshProUGUI scoreText;
+    public Text highscoreText;
+    public Text scoreText;
+    public Text spellText;
 
     int score = 0;
     int highscore = 0;
+    string holdingSpell = null;
     
     void Start()
     {
@@ -37,11 +39,32 @@ public class GameController : MonoBehaviour
         score++;
         UpdateScore();
     }
+
+    public void PickedUpSpell(string spell)
+    {
+        spellText.gameObject.SetActive(true);
+        int cost = 5;
+        spellText.text = $"{spell} ({cost})";
+        holdingSpell = spell;
+        if (score >= cost) spellText.color = Color.white;
+        else spellText.color = new Color(1f,1f,1f,0f);
+    }
+
+    public void UsedSpell()
+    {
+        score -= 5;
+        holdingSpell = null;
+        spellText.gameObject.SetActive(false);
+    }
     
     void UpdateScore()
     {
         scoreText.text = score.ToString();
         highscoreText.text = highscore.ToString();
+        if (holdingSpell == null) return;
+        // possibly makes spell available if enough score
+        if (score >= 5) spellText.color = Color.white;
+        else spellText.color = new Color(1f,1f,1f,0f);
     }
 
     void UpdateHighscore()
